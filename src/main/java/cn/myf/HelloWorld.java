93d9b1e262b1186d77364e3ff37d90053cd54094
Doris <603175593@qq.com>
2020-05-03 20:16:10 +0800

    git修改提交

---
+++ src/main/java/cn/myf/HelloWorld.java
@@ -0,0 +1,21 @@
+package cn.myf;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        test();
+        System.out.println("git");
+        System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
+    }
+    private static void test(){
+        System.out.println("gittest");
+        System.out.println("llllll");
+        //三生三世
+    }
+    private static void test2(){
+        System.out.println("222222");
+    }
+
+}
