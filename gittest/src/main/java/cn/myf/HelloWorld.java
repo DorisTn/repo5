638ee10fce8b0ddc58e2a81ffc5296eba6b2f2bc
Doris <603175593@qq.com>
2020-05-03 17:34:46 +0800

    修改了之后的HelloWorld02

--- gittest/src/main/java/cn/myf/HelloWorld.java
+++ gittest/src/main/java/cn/myf/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         test();
+        System.out.println("git");
     }
     private static void test(){
         System.out.println("gittest");
