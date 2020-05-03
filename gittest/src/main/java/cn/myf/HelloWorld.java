0de0dd689dee7bc92688d7e3b1ee6449d2b03b56
Doris <603175593@qq.com>
2020-05-03 16:59:00 +0800

    修改了之后的HelloWorld

--- gittest/src/main/java/cn/myf/HelloWorld.java
+++ gittest/src/main/java/cn/myf/HelloWorld.java
@@ -3,6 +3,10 @@ package cn.myf;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        test();
+    }
+    private static void test(){
+        System.out.println("gittest");
     }
 
 }
